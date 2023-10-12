e805f09758fdbdb1bbdba9a714212524d99d32a8
carlosbernat <69471013+carlosbernat@users.noreply.github.com>
2023-10-12 10:48:20 -0500

    Commit Challenge 5

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,29 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useCounter } from './components/useCounter';
 
-const FirstApp = ({value}) => {
 
-  const [counter, setCounter] = useState(value);
-
- 
-  const handleSubtract = () => {
-    
-    setCounter(counter - 1);
-  };
-
-  
-  const handleReset = () => {
-  
-    setCounter(value);
-  };
+const FirstApp = ({ value }) => {
+  const { count, increment, decrement, reset } = useCounter();
 
   return (
     <div>
-      <h1>Nuevo Título</h1>
-      <span>{counter}</span> {}
+      <h1>Contador Challenge 5</h1>
+      <span>{count}</span>
       <br />
-      <button onClick={handleSubtract}>Restar</button>
-      <button onClick={handleReset}>Reiniciar</button> 
+      <button onClick={decrement}>Restar -1</button>
+      <button onClick={reset}>Reiniciar</button>
+      <button onClick={increment}>Sumar +1</button>
     </div>
-  )
-}
+  );
+};
+
 export default FirstApp;
