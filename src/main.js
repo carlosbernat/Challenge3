c8a05ed8dc1e4e4468a392655e7c23c1d6f00cac
carlosbernat <69471013+carlosbernat@users.noreply.github.com>
2023-11-23 11:22:56 -0500

    challenge16

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./store/store";
-import { Registro } from "./Registro";
-import { Upload } from "./Upload";
+import { Crud } from "./crud";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <Provider>
     <BrowserRouter>
-      <Registro />
-      <Upload />
+      <Crud />
     </BrowserRouter>
   </Provider>
 );
